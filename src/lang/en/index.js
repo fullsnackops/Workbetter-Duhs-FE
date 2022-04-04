2ddb10e36a2cd43169e843033f1a862fbcec9765
fullsnackops <tbsniper61@gmail.com>
2022-04-04 10:31:32 -0400

    userNotFound exception

--- src/lang/en/index.js
+++ src/lang/en/index.js
@@ -14,4 +14,6 @@ export default {
     analytics: 'Analytics',
     analyticsmeeting: 'Analytics Meeting',
     userProfile: 'User Profile',
+    userNotFound:
+        'Uh-oh. Workbetter-Duhs could not find a user account associated with the provided Google user. Please make sure that you have selected the correct Google user or if you are new to Workbetter-Duhs, please access our sign up page to create a new account.',
 }
