0fea22c95601dfbcf808bd74d7d8853912453c0d
fullsnackops <tbsniper61@gmail.com>
2022-04-04 12:10:46 -0400

    update pages routing

--- src/router/index.js
+++ src/router/index.js
@@ -4,40 +4,9 @@ import Router from 'vue-router'
 // routes
 import routes from './routes'
 
-// Session Components
-const SignUp = () => import('@/pages/session/SignUp')
-const Login = () => import('@/pages/session/Login')
-const NotFound = () => import('@/pages/NotFound')
-
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
-    routes: [
-        routes,
-        {
-            path: '/session/sign-up',
-            component: SignUp,
-            meta: {
-                title: 'message.signUp',
-                breadcrumb: 'Session / Sign Up',
-            },
-        },
-        {
-            path: '/session/login',
-            component: Login,
-            meta: {
-                title: 'message.login',
-                breadcrumb: 'Session / Login',
-            },
-        },
-        {
-            path: '*',
-            component: NotFound,
-            meta: {
-                title: 'message.pageNotFound',
-                breadcrumb: 'Page Not Found',
-            },
-        },
-    ],
+    routes,
 })
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,22 +1,63 @@
-import defaultContainer from '@/container/default'
+// Session Components
+const SignUp = () => import('@/pages/session/SignUp')
+const Login = () => import('@/pages/session/Login')
 
 // Reports Components
 const AnalyticsMeeting = () => import('@/pages/main/AnalyticsMeeting')
 
-export default {
-    path: '/',
-    component: defaultContainer,
-    redirect: '/main/analytics-meeting',
-    children: [
-        {
-            path: '/main/analytics-meeting',
-            name: 'analytics-meeting',
-            component: AnalyticsMeeting,
-            meta: {
-                requiresAuth: true,
-                title: 'message.analyticsmeeting',
-                breadcrumb: 'Analytics / Meeting',
-            },
+// Other
+const NotFound = () => import('@/pages/NotFound')
+
+export default [
+    {
+        path: '/',
+        name: 'home',
+        redirect: '/main/analytics-meeting',
+    },
+
+    // Session
+    {
+        path: '/session/sign-up',
+        component: SignUp,
+        meta: {
+            title: 'message.signUp',
+            breadcrumb: 'Session / Sign Up',
+            layout: 'preauth',
         },
-    ],
-}
+    },
+    {
+        path: '/session/login',
+        name: 'login',
+        component: Login,
+        meta: {
+            title: 'message.login',
+            breadcrumb: 'Session / Login',
+            layout: 'preauth',
+        },
+    },
+
+    // Analytics Meeting
+    {
+        path: '/main/analytics-meeting',
+        name: 'analytics-meeting',
+        component: AnalyticsMeeting,
+        meta: {
+            requiresAuth: true,
+            title: 'message.analyticsmeeting',
+            page: 'analytics-meeting',
+            type: 'meeting',
+            breadcrumb: 'Analytics / Meeting',
+            layout: 'preauth',
+        },
+    },
+
+    // 404
+    {
+        path: '*',
+        component: NotFound,
+        meta: {
+            title: 'Page Not Found',
+            breadcrumb: 'Page Not Found',
+        },
+    },
+]
