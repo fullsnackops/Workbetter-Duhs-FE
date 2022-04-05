008af1a9e929183e9ea5c7392cbab89efacfcdae
fullsnackops <tbsniper61@gmail.com>
2022-04-05 17:55:32 -0400

    add aws user pool client id

--- config/env-vars.js
+++ config/env-vars.js
@@ -11,8 +11,8 @@ const dev = {
   googleApiKey: 'AIzaSyCQTCwatk156bD6LP5F117aqJvwztnulzE',
   googleClientId: '451062396599-30bfeuspln7i19c1ckgf2sf9uv93s44i.apps.googleusercontent.com',
   baseURI: '',
-  UserPoolId: 'us-east-1_vpsErMsv1',
-  UserPoolAppClientId: '3kluub58bs92ugid89elhp1u1s',
+  UserPoolId: 'us-east-1_wF0XviU1P',
+  UserPoolAppClientId: '7in5bn864v1hnkiub9b90evrra',
   AmplitudeToken: '73c8e58bfa7a75503760b83e0a091e54'
 }
 
