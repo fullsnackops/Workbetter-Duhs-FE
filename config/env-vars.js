1846fa3e4afa1c544aa9b9bb91383f79292e12ed
fullsnackops <tbsniper61@gmail.com>
2022-04-08 15:51:47 -0400

    update google client id, add backend dev uri

--- config/env-vars.js
+++ config/env-vars.js
@@ -9,8 +9,8 @@ const prod = {
 
 const dev = {
   googleApiKey: 'AIzaSyCQTCwatk156bD6LP5F117aqJvwztnulzE',
-  googleClientId: '451062396599-30bfeuspln7i19c1ckgf2sf9uv93s44i.apps.googleusercontent.com',
-  baseURI: '',
+  googleClientId: '451062396599-chf887krbe1b7a2rll672rf9r6c9sv98.apps.googleusercontent.com',
+  baseURI: 'http://localhost:3000',
   UserPoolId: 'us-east-1_wF0XviU1P',
   UserPoolAppClientId: '7in5bn864v1hnkiub9b90evrra',
   AmplitudeToken: '73c8e58bfa7a75503760b83e0a091e54'
