82a1bcd90be817f8f7a1202c0dfeb34b6097b837
fullsnackops <tbsniper61@gmail.com>
2022-04-17 07:34:28 -0400

    add sidebar, lng for w, m

--- src/lang/en/index.js
+++ src/lang/en/index.js
@@ -14,8 +14,11 @@ export default {
     importingevents: 'IMPORTING EVENTS...',
     pageNotFound: 'Page Not Found',
     backToWork: 'Back to Work',
-    analytics: 'Analytics',
-    analyticsmeeting: 'Analytics Meeting',
+    weeklyDashboards: 'Weekly Dashboards',
+    weeklyAnalyzer: 'W - Analyzer',
+    weeklyPlanner: 'W - Planner',
+    monthlyDashboards: 'Monthly Dashboards',
+    monthlyAnalyzer: 'M - Analyzer',
     userProfile: 'User Profile',
     userNotFound:
         'Uh-oh. Workbetter-Duhs could not find a user account associated with the provided Google user. Please make sure that you have selected the correct Google user or if you are new to Workbetter-Duhs, please access our sign up page to create a new account.',
