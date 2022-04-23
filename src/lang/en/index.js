12aafe83bf04211c8651aa6831e3f472b6c7c386
fullsnackops <tbsniper61@gmail.com>
2022-04-23 10:48:33 -0400

    add lng for MeetingStats, MeetingTypes

--- src/lang/en/index.js
+++ src/lang/en/index.js
@@ -1,7 +1,12 @@
 // messages.js
 export default {
+    // General
     pages: 'Pages',
     search: 'Search',
+    fullScreen: 'Full Screen',
+    save: 'Save',
+
+    // Login & Sign up
     login: 'Login',
     logOut: 'LogOut',
     loginToDashboard: 'Login to Workbetter-Duhs',
@@ -14,12 +19,34 @@ export default {
     importingevents: 'IMPORTING EVENTS...',
     pageNotFound: 'Page Not Found',
     backToWork: 'Back to Work',
+    userProfile: 'User Profile',
+    userNotFound:
+        'Uh-oh. Workbetter-Duhs could not find a user account associated with the provided Google user. Please make sure that you have selected the correct Google user or if you are new to Workbetter-Duhs, please access our sign up page to create a new account.',
+
+    // Sidebar
     weeklyDashboards: 'Weekly Dashboards',
     weeklyAnalyzer: 'W - Analyzer',
     weeklyPlanner: 'W - Planner',
     monthlyDashboards: 'Monthly Dashboards',
     monthlyAnalyzer: 'M - Analyzer',
-    userProfile: 'User Profile',
-    userNotFound:
-        'Uh-oh. Workbetter-Duhs could not find a user account associated with the provided Google user. Please make sure that you have selected the correct Google user or if you are new to Workbetter-Duhs, please access our sign up page to create a new account.',
+
+    // Meeting Stats
+    meetingStats: 'Meeting Stats',
+    lastWeek: 'Week Of',
+    last30Days: 'Previous 30 Days',
+    total: 'TOTAL',
+    average: 'AVERAGE',
+    attended: 'Attended',
+    attendedTip: 'Meetings you attended but did not organize.',
+    organized: 'Organized',
+    organizedTip: 'Meetings that you have organized',
+
+    // Meeting Types
+    meetingTypes: 'Meeting Types',
+    recurringVsOneTimeMeetings: 'Recurring vs. One-Time Meetings',
+    recurringMeetings: 'Recurring Meetings',
+    oneTimeMeetings: 'One-Time Meetings',
+    externalVsInternalMeetings: 'External vs. Internal Meetings',
+    externalMeetings: 'External Meetings',
+    internalMeetings: 'Internal Meetings',
 }
