e68207cd092df902502b938261c70af7fbcc1c17
fullsnackops <tbsniper61@gmail.com>
2022-05-06 05:31:09 -0400

    add Planner container

--- src/pages/weekly/Planner.vue
+++ src/pages/weekly/Planner.vue
@@ -1,3 +1,19 @@
 <template>
-    <div />
+    <v-container fluid grid-list-xl>
+        <v-layout row wrap border-rad-sm overflow-hidden>
+            <app-card :heading="$t('message.meetingTimeCalendar')" colClasses="xs12" :fullScreen="true" />
+        </v-layout>
+    </v-container>
 </template>
+
+<script>
+export default {
+    name: 'WeeklyPlanner',
+    mounted: function() {
+        this.$store.dispatch('viewDashboard', {
+            dashboard: 'Weekly Planner',
+            dateRange: this.currentWeek(),
+        })
+    },
+}
+</script>
