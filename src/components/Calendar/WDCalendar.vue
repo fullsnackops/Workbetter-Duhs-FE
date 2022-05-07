e4759997ed2fe7c200fc783fb55ec15d2be5859d
fullsnackops <tbsniper61@gmail.com>
2022-05-07 11:51:11 -0400

    add tempo WDCalendar component

---
+++ src/components/Calendar/WDCalendar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        Workbetter-Duhs Calendar
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WDCalendar',
+    props: {
+        appointments: {
+            required: true,
+            type: Array,
+        },
+        configuration: {
+            type: Object,
+            required: false,
+            validator: val => {
+                return typeof val === 'object'
+            },
+        },
+    },
+}
+</script>
