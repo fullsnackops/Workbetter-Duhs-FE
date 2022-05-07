ca80987e77f9c587966ca61ff58fdb18f41921f6
fullsnackops <tbsniper61@gmail.com>
2022-05-07 11:48:05 -0400

    add WDCalendar component

--- src/components/Widgets/WeekLookAheadView.vue
+++ src/components/Widgets/WeekLookAheadView.vue
@@ -4,7 +4,9 @@
         <v-container fluid grid-list-xl>
             <v-layout row wrap>
                 <v-flex xs12 py-0>
-                    <skeleton-list :data="events" :repeat="15" :enable-animation="true" />
+                    <skeleton-list :data="events" :repeat="15" :enable-animation="true">
+                        <w-d-calendar :configuration="config" :appointments="events" />
+                    </skeleton-list>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -12,10 +14,14 @@
 </template>
 
 <script>
+import WDCalendar from '@/components/Calendar/WDCalendar'
 import { config } from '@/components/Calendar/CalendarConfig'
 
 export default {
     name: 'WeekLookAheadView',
+    components: {
+        WDCalendar,
+    },
     props: ['events'],
     data() {
         return {
