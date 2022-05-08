6ad9f7c9fb5f4dcd852d71372ff2e57495a2e39c
fullsnackops <tbsniper61@gmail.com>
2022-05-08 12:46:54 -0400

    add generating calendar properties

--- src/components/Calendar/WDCalendar.vue
+++ src/components/Calendar/WDCalendar.vue
@@ -1,14 +1,31 @@
 <template>
-    <div>
-        Workbetter-Duhs Calendar
+    <div
+        class="calendarium wrapping-all"
+        :class="{
+            dark: !!calendar_options.dark,
+        }"
+    >
+        <w-d-calendar-week :days="calendar_options.current_week" :hours="hours"></w-d-calendar-week>
     </div>
 </template>
 
 <script>
+import { addDays, startOfWeek, endOfWeek, format, getTime, addHours, addMinutes } from 'date-fns'
+import WDCalendarWeek from './WDCalendarWeek'
 import moment from 'moment-timezone'
+
+let timezoneOffset = 0
+let slotStep = 1 * 60 * 1000
+
+function getSlotTime(time) {
+    return Math.round((time - timezoneOffset) / slotStep) * slotStep
+}
 
 export default {
     name: 'WDCalendar',
+    components: {
+        WDCalendarWeek,
+    },
     props: {
         appointments: {
             required: true,
@@ -35,6 +52,15 @@ export default {
         }
     },
     computed: {
+        hourParts() {
+            return (
+                (60 / this.calendar_options.split_value) *
+                (this.calendar_options.end_hours - this.calendar_options.start_hours)
+            )
+        },
+        view_type() {
+            return this.configuration.view_type
+        },
         calendar_options: {
             get() {
                 let options = this.default_options
@@ -69,6 +95,9 @@ export default {
         }
         this.hours = visibleHours
     },
+    mounted() {
+        this.generateCalendarProperties()
+    },
     provide() {
         const provider = {}
         Object.defineProperty(provider, 'calendarOptions', {
@@ -77,5 +106,124 @@ export default {
         })
         return provider
     },
+    methods: {
+        constructWeek() {
+            // const { timezone } = this.$store.getters.user
+            const timezone = 'America/Chicago'
+            const utcOffset = new Date().getTimezoneOffset()
+            timezoneOffset = (moment.tz(timezone).zone() - utcOffset) * 60 * 1000
+            slotStep = this.calendar_options.split_value * 60 * 1000
+            let filteredAppointments = []
+            let date = new Date(this.calendar_options.current_day)
+            date.setHours(0, 0, 0, 0)
+            let _days = []
+            let start = this.calendar_options.view_type === 'Week' ? this.startOfTheWeek(date) : format(date)
+            let numOfDays = this.calendar_options.view_type === 'Week' ? 5 : 1
+            for (let i = 0; i < numOfDays; i++) {
+                let _date = addHours(addDays(start, i), this.calendar_options.start_hours)
+                let payload = {
+                    date: format(_date, 'YYYY-MM-DD'),
+                }
+                payload.date_hours = new Array(this.hourParts).fill(1).map((hour, index) => {
+                    return {
+                        value: index > 0 ? addMinutes(_date, index * this.calendar_options.split_value) : _date,
+                        index,
+                    }
+                })
+
+                for (const appointment of this.appointments) {
+                    if (appointment.date !== payload.date) continue
+                    let targetHourFrom = payload.date_hours.find(
+                        hour => getTime(hour.value) === getSlotTime(appointment.from)
+                    )
+                    if (!targetHourFrom && getSlotTime(appointment.from) < getTime(payload.date_hours[0].value)) {
+                        targetHourFrom = payload.date_hours[0]
+                    }
+                    let targetHourTo = payload.date_hours.find(
+                        hour => getTime(hour.value) === getSlotTime(appointment.to)
+                    )
+                    if (
+                        !targetHourTo &&
+                        getSlotTime(appointment.to) > getTime(payload.date_hours[payload.date_hours.length - 1].value)
+                    ) {
+                        targetHourTo = payload.date_hours[payload.date_hours.length - 1]
+                    }
+                    if (!targetHourFrom || !targetHourTo) continue
+                    filteredAppointments.push({
+                        id: appointment.id,
+                        start: targetHourFrom.index,
+                        end: targetHourTo.index - 1,
+                        date: appointment.date,
+                        data: {
+                            ...appointment.data,
+                            from: moment.tz(appointment.from, timezone).format(),
+                            to: moment.tz(appointment.to, timezone).format(),
+                        },
+                        status: 'completed',
+                    })
+                }
+                _days.push(payload)
+            }
+            return {
+                filteredAppointments: filteredAppointments,
+                _days: _days,
+            }
+        },
+        formatDate(_format, how) {
+            return format(_format, how)
+        },
+        startOfTheWeek(value) {
+            const sun = startOfWeek(value, {
+                weekStartsOn: 0,
+            })
+            sun.setTime(sun.getTime() + 24 * 60 * 60 * 1000)
+            return sun
+        },
+        endOfTheWeek(value) {
+            const sat = endOfWeek(value, {
+                weekStartsOn: 0,
+            })
+            sat.setTime(sat.getTime() - 24 * 60 * 60 * 1000)
+            return sat
+        },
+        generateCalendarProperties() {
+            let { filteredAppointments, _days } = this.constructWeek()
+            this.$set(this.calendar_options, 'existing_appointments', filteredAppointments)
+            this.weeks[_days[0].date] = _days
+            this.$set(this.calendar_options, 'current_week', _days)
+        },
+        getHours(start, end) {
+            return `${format(start, 'HH:mm')} - ${format(end, 'HH:mm')}`
+        },
+    },
+    watch: {
+        appointments(val) {
+            if (val) this.generateCalendarProperties()
+        },
+        view_type(val) {
+            if (['Week', 'Day'].includes(val)) {
+                this.generateCalendarProperties()
+            }
+        },
+        split_value(val) {
+            if (60 % parseInt(val) === 0) this.generateCalendarProperties()
+        },
+        current_day(val) {
+            if (val) this.generateCalendarProperties()
+        },
+    },
 }
 </script>
+
+<style lang="scss" scoped>
+.calendarium {
+    &.non-desktop .building-blocks {
+        pointer-events: none;
+    }
+}
+
+.week-navigator {
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
