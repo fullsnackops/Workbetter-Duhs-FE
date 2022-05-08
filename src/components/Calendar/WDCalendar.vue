e0c7204560077635bb01d9ef6e3d3a174a1bb6af
fullsnackops <tbsniper61@gmail.com>
2022-05-08 06:00:34 -0400

    integrate dynamic calendar options as computed

--- src/components/Calendar/WDCalendar.vue
+++ src/components/Calendar/WDCalendar.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import moment from 'moment-timezone'
+
 export default {
     name: 'WDCalendar',
     props: {
@@ -17,6 +19,41 @@ export default {
             required: false,
             validator: val => {
                 return typeof val === 'object'
+            },
+        },
+    },
+    data() {
+        return {
+            default_options: {
+                current_week: null,
+                currently_working_on_date: null,
+                current_day: null,
+                existing_appointments: {},
+            },
+            weeks: {},
+        }
+    },
+    computed: {
+        calendar_options: {
+            get() {
+                let options = this.default_options
+                let providedProps = this.configuration
+                // const { timezone } = this.$store.getters.user
+                const timezone = 'America/Chicago'
+                const today = moment.tz(timezone)
+                if (today.day() > 5 || (today.day() === 5 && today.hours() > 11)) {
+                    today.add(1, 'week').startOf('week')
+                }
+                today.add(this.$store.getters.wplannerOffset, 'weeks')
+
+                options.current_day = today
+                for (let key in providedProps) {
+                    options[key] = providedProps[key]
+                }
+                return options
+            },
+            set(val) {
+                this.configuration = val
             },
         },
     },
