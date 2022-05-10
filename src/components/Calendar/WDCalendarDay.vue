50c7bd7ae26a07d0abe125dca8b0466111217c3f
fullsnackops <tbsniper61@gmail.com>
2022-05-10 05:04:58 -0400

    build temp calendar day component

---
+++ src/components/Calendar/WDCalendarDay.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="mf-calendar-day">
+        <!-- Calendar cell(quarter) view -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WDCalendarDay',
+    props: ['day'],
+    inject: ['calendarOptions'],
+}
+</script>
