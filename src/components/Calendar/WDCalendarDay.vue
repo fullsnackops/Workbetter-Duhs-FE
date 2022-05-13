aa83f42b5bbb0f6460b7f25440826e0c229011a4
fullsnackops <tbsniper61@gmail.com>
2022-05-13 06:49:18 -0400

    import CalendarCell Component

--- src/components/Calendar/WDCalendarDay.vue
+++ src/components/Calendar/WDCalendarDay.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="mf-calendar-day">
         <!-- Calendar cell(quarter) view -->
+        <w-d-calendar-cell
+            v-for="(quarter, index) in day.date_hours"
+            :key="`${day.date}_${index}`"
+            :day="day"
+            :index="index"
+            :cell-data.sync="quarter"
+        />
     </div>
 </template>
 
 <script>
+import WDCalendarCell from './WDCalendarCell'
+
 export default {
     name: 'WDCalendarDay',
     props: ['day'],
     inject: ['calendarOptions'],
+    components: {
+        WDCalendarCell,
+    },
 }
 </script>
