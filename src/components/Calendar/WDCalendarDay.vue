13598bfc5959aaed4c419ae5524b0fdae6208b59
fullsnackops <tbsniper61@gmail.com>
2022-05-14 21:36:56 -0400

    handle overlapping events correctly

--- src/components/Calendar/WDCalendarDay.vue
+++ src/components/Calendar/WDCalendarDay.vue
@@ -1,25 +1,56 @@
 <template>
-    <div class="mf-calendar-day">
-        <!-- Calendar cell(quarter) view -->
-        <w-d-calendar-cell
-            v-for="(quarter, index) in day.date_hours"
-            :key="`${day.date}_${index}`"
-            :day="day"
-            :index="index"
-            :cell-data.sync="quarter"
-        />
-    </div>
+  <div class="mf-calendar-day">
+    <!-- Calendar cell(quarter) view -->
+    <m-f-calendar-cell
+      v-for="quarter in day.date_hours"
+      :key="`${day.date}_${quarter.index}`"
+      :cell-data.sync="quarter"
+      :dayTentatives="sortedTentatives"
+    ></m-f-calendar-cell>
+  </div>
 </template>
 
 <script>
-import WDCalendarCell from './WDCalendarCell'
+import isSameDay from 'date-fns/is_same_day'
+import orderBy from 'lodash/orderBy'
+import isEmpty from 'lodash/isEmpty'
+import difference from 'lodash/difference'
+import MFCalendarCell from './MFCalendarCell'
 
 export default {
-    name: 'WDCalendarDay',
-    props: ['day'],
-    inject: ['calendarOptions'],
-    components: {
-        WDCalendarCell,
+  props: ['day'],
+  inject: ['calendarOptions'],
+  components: {
+    MFCalendarCell
+  },
+  computed: {
+    dayTentatives () {
+      const { date } = this.day
+      const { existing_appointments: appointments } = this.calendarOptions
+      return orderBy(appointments.filter(appointment => appointment.data.category === 'tentative' && isSameDay(appointment.data.from, date)), ['start'])
     },
+    sortedTentatives () {
+      let blocks = [...this.dayTentatives]
+      let sortedBlocks = []
+      while (!isEmpty(blocks)) {
+        let rblocks = []
+        for (const block of blocks) {
+          if (isEmpty(rblocks) || (!isEmpty(rblocks) && rblocks[rblocks.length - 1].end <= block.start)) {
+            rblocks.push(block)
+          }
+        }
+        sortedBlocks.push(rblocks)
+        blocks = [...difference(blocks, rblocks)]
+      }
+      return sortedBlocks
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.mf-calendar-day {
+  position: relative;
+  display: flex;
+}
+</style>
