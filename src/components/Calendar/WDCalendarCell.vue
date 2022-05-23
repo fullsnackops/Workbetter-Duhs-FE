f74404f8a37b9a3638a188882f464e54ee3d6bb8
fullsnackops <tbsniper61@gmail.com>
2022-05-23 17:24:50 -0400

    update cell component

--- src/components/Calendar/WDCalendarCell.vue
+++ src/components/Calendar/WDCalendarCell.vue
@@ -21,43 +21,113 @@
             }"
             :style="appointmentStyle(aIndex)"
         >
-            <div v-if="appointment.data" class="existing-event">
-                <template
-                    v-if="
-                        appointment.data.category === 'meeting' ||
-                            appointment.data.category === 'ooo' ||
-                            appointment.data.category === 'tentative'
-                    "
+            <v-tooltip
+                color="white"
+                top
+                content-class="cellTooltip"
+                nudge-top="-8"
+                :disabled="!(appointment.data.category === 'meeting')"
+                open-delay="1000"
+            >
+                <div
+                    slot="activator"
+                    v-if="appointment.data"
+                    class="existing-event"
+                    ref="event"
+                    @click="selectEvent(appointment.id)"
                 >
-                    <cell-title :appointment="appointment" :style="appointmentTitleStyle(aIndex)" />
-                    <small v-if="calendarOptions.show_description && appointment.end - appointment.start > 2">
-                        {{ appointment.data.description }}
-                    </small>
-                    <span class="time" v-if="calendarOptions.show_hours">
-                        {{ appointment.data.from | normalizeDate('hh:mm A') }} -
-                        {{ appointment.data.to | normalizeDate('hh:mm A') }}
-                    </span>
-                    <div class="buttons" v-if="false && appointment.data.category === 'meeting'">
-                        <v-btn flat icon small><v-icon class="font-sm" color="primary">group</v-icon></v-btn>
-                        <v-btn flat icon small><v-icon class="font-sm" color="primary">videocam</v-icon></v-btn>
-                    </div>
-                </template>
-            </div>
+                    <template
+                        v-if="
+                            appointment.data.category === 'meeting' ||
+                                appointment.data.category === 'ooo' ||
+                                appointment.data.category === 'tentative'
+                        "
+                    >
+                        <cell-title-meeting
+                            :appointment="appointment"
+                            :astyle="appointmentTitleStyle(aIndex)"
+                            v-if="
+                                appointment.data.category === 'meeting' &&
+                                    (appointment.data.tag_critical || appointment.data.tag_personal)
+                            "
+                            :eventHeight="eventHeight(aIndex)"
+                        />
+                        <cell-title :appointment="appointment" :astyle="appointmentTitleStyle(aIndex)" v-else />
+                        <small v-if="calendarOptions.show_description && appointment.end - appointment.start > 2">
+                            {{ appointment.data.description }}
+                        </small>
+                        <span class="time" v-if="calendarOptions.show_hours">
+                            {{ normalizeTime(appointment.data.from) }}
+                            -
+                            {{ normalizeTime(appointment.data.to) }}
+                        </span>
+                    </template>
+                </div>
+                <span v-if="appointment.data.category === 'meeting'">
+                    <template
+                        v-if="appointment.data.category === 'focus' || appointment.data.category === 'transition'"
+                    >
+                        <p class="ma-2">
+                            <template v-if="appointment.data.category === 'focus'">
+                                Focus Time:
+                            </template>
+                            <template v-else>
+                                Transition Time:
+                            </template>
+                            {{ hours(aIndex) | normalizeNumber(2) }} hours<br />
+                            ({{ normalizeTime(appointment.data.from) }} - {{ normalizeTime(appointment.data.to) }})
+                        </p>
+                    </template>
+                    <p v-else-if="appointment.data.category === 'meeting'" class="ma-2">
+                        <strong>{{ eventTitle(appointment.data.title) }}</strong>
+                        <br />
+                        {{ normalizeTime(appointment.data.from) }}
+                        -
+                        {{ normalizeTime(appointment.data.to) }}
+                        ({{ hours(aIndex) | normalizeNumber(2) }} hours)
+                        <br />
+                        Organizer: {{ organizer(aIndex) }}<br />
+                        <template v-if="appointment.data.tag_critical || appointment.data.tag_personal">
+                            <br />
+                            <span>
+                                <v-icon>fa fa-user{{ appointment.data.tag_critical ? 's' : '' }}</v-icon>
+                                {{
+                                    appointment.data.tag_critical
+                                        ? 'Workbetter-Duhs Critical Meeting'
+                                        : 'Workbetter-Duhs Personal Meeting'
+                                }}
+                            </span>
+                        </template>
+                    </p>
+                </span>
+            </v-tooltip>
         </div>
     </div>
 </template>
 
 <script>
-import isSameDay from 'date-fns/is_same_day'
-import CalendarActions from './CalendarActions'
-import CellTitle from './MFCalendarCellTitle'
+import { isEmpty } from 'lodash'
+import { EventBus } from '@/lib/eventBus'
+import { isSameDay, differenceInMinutes } from 'date-fns'
+import moment from 'moment-timezone'
+import CellTitle from './WDCalendarCellTitle'
+import CellTitleMeeting from './WDCalendarCellTitleMeeting'
+import AppConfig from '@/constants/AppConfig'
+
 export default {
+    name: 'WDCalendarCell',
     components: {
-        CalendarActions,
         CellTitle,
+        CellTitleMeeting,
     },
-    props: ['cellData', 'dayTentatives'],
+    props: ['cellData', 'dayTentatives', 'tentativeColumnCount'],
     inject: ['calendarOptions'],
+    data() {
+        return {
+            TAG_CRITICAL: AppConfig.meetingTag.critical,
+            TAG_PERSONAL: AppConfig.meetingTag.personal,
+        }
+    },
     computed: {
         cellAppointments() {
             const { cellData } = this
@@ -71,9 +141,28 @@ export default {
         },
     },
     methods: {
+        eventTitle(title) {
+            if (title.includes(this.TAG_CRITICAL)) {
+                return title.replace(this.TAG_CRITICAL, '')
+            }
+
+            if (title.includes(this.TAG_PERSONAL)) {
+                return title.replace(this.TAG_PERSONAL, '')
+            }
+
+            return title
+        },
         isCategory(category, index) {
             const { cellAppointments } = this
             return cellAppointments[index] && cellAppointments[index].data.category === category
+        },
+        hours(index) {
+            const { cellAppointments } = this
+            if (!cellAppointments[index]) {
+                return false
+            }
+            const mins = differenceInMinutes(cellAppointments[index].data.to, cellAppointments[index].data.from)
+            return mins / 60
         },
         distance(index) {
             const { cellAppointments } = this
@@ -88,21 +177,34 @@ export default {
             let right = '1px'
             let height = this.distance(index) + 'px'
             if (this.isCategory('tentative', index)) {
-                const { dayTentatives, cellAppointments } = this
+                const { tentativeColumnCount, dayTentatives, cellAppointments } = this
                 const appointment = cellAppointments[index]
-                let tCount = dayTentatives.length
+                let tCount = tentativeColumnCount
                 let tIndex = dayTentatives.reduce(
                     (tNum, tentatives, hIndex) => {
                         if (tentatives.includes(appointment)) {
-                            tNum = { h: hIndex, v: tentatives.indexOf(appointment) }
+                            tNum = {
+                                h: hIndex,
+                                v: tentatives.indexOf(appointment),
+                            }
                         }
                         return tNum
                     },
-                    { h: 0, v: 0 }
+                    {
+                        h: 0,
+                        v: 0,
+                    }
                 )
                 if (tCount < 2) tCount = 2
-                left = `${100 - ((tIndex.h + 1) * 100) / tCount}%`
-                right = `${(tIndex.h * 100) / tCount}%`
+                if (tCount > 3) {
+                    tCount = tCount - 1
+                    const tentativeWidth = (100 * 2) / 3
+                    left = `${100 - ((tIndex.h + 1) * tentativeWidth) / tCount}%`
+                    right = `${(tIndex.h * tentativeWidth) / tCount}%`
+                } else {
+                    left = `${100 - ((tIndex.h + 1) * 100) / tCount}%`
+                    right = `${(tIndex.h * 100) / tCount}%`
+                }
             }
             return {
                 left,
@@ -112,24 +214,77 @@ export default {
         },
         appointmentTitleStyle(index) {
             let width = '100%'
-            const { dayTentatives, cellAppointments } = this
+            const { tentativeColumnCount, dayTentatives, cellAppointments } = this
             const appointment = cellAppointments[index]
-            let tCount = dayTentatives.length
+            let tCount = tentativeColumnCount
             if (tCount < 2) tCount = 2
             let overlappings = 0
             if (this.isCategory('meeting', index) || this.isCategory('ooo', index)) {
                 for (const tentatives of dayTentatives) {
                     for (const tentative of tentatives) {
-                        if (tentative.start < appointment.start && tentative.end > appointment.start) {
+                        if (tentative.start <= appointment.start && tentative.end > appointment.start) {
                             overlappings = overlappings + 1
                         }
                     }
                 }
+                let meetingTitleWidth = 100 - (overlappings * 100) / tCount
+                if (meetingTitleWidth < (100 * 1) / 3) meetingTitleWidth = (100 * 1) / 3
+                width = `${meetingTitleWidth}%`
+            } else {
+                width = '100%'
             }
-            width = `${100 - (overlappings * 100) / tCount}%`
             return {
                 width,
             }
+        },
+        eventHeight(index) {
+            return () => {
+                return this.$refs.event && this.$refs.event[index] ? this.$refs.event[index].clientHeight : 0
+            }
+        },
+        selectEvent(eventId) {
+            EventBus.$emit('clickEvent', { eventId, from: 'calendar' })
+        },
+        organizer(index) {
+            const appointment = this.cellAppointments[index]
+            if (!appointment) return '-'
+            if (appointment.data.isOrganizer) {
+                return this.$store.getters.user.name
+            } else {
+                const { attendees } = appointment.data
+                if (!isEmpty(attendees)) {
+                    const attender = attendees.find(attendee => attendee.org === 1)
+                    if (!isEmpty(attender)) {
+                        if (attender.name) return attender.name
+                        const recapPeers =
+                            !isEmpty(this.$store.getters.recap) && !isEmpty(this.$store.getters.recap.attendees.peers)
+                                ? this.$store.getters.recap.attendees.peers
+                                : null
+                        const plannerPeers =
+                            !isEmpty(this.$store.getters.planner) &&
+                            !isEmpty(this.$store.getters.planner.attendees.peers)
+                                ? this.$store.getters.planner.attendees.peers
+                                : null
+                        const peers = recapPeers || plannerPeers
+                        if (peers) {
+                            const peerAttender = peers.find(attendee => attendee.email === attender.email)
+                            if (!isEmpty(peerAttender)) {
+                                return peerAttender.name
+                            }
+                        }
+                        if (attender.email) return attender.email
+                    }
+                }
+            }
+            return '-'
+        },
+        normalizeTime(date) {
+            // const timezone = this.$store.getters.user.timezone
+            const timezone = 'America/Chicago'
+            if (date) {
+                return moment.tz(date, timezone).format('h:mm A')
+            }
+            return '?'
         },
     },
 }
@@ -151,10 +306,6 @@ $focus-color: rgba(
 );
 $ooo-color: rgba(
     $color: $gray-dark,
-    $alpha: $alpha,
-);
-$meeting-color: rgba(
-    $color: $meeting-color,
     $alpha: $alpha,
 );
 $tentative-color: rgba(0, 0, 0, 0.05);
@@ -197,6 +348,7 @@ $non-working-color: darken(
             background-color: #bfecff;
             opacity: 1;
             color: #1f6570;
+            overflow: hidden;
             p,
             span {
                 margin: 0;
@@ -235,7 +387,7 @@ $non-working-color: darken(
             }
         }
         &.is_focus .existing-event {
-            background: $focus-color url('/static/logos/logo-white.png') no-repeat;
+            background: $focus-color url('/static/logos/logo.png') no-repeat;
             background-size: 34px 34px;
             background-position: center;
         }
@@ -276,5 +428,21 @@ $non-working-color: darken(
             color: darken($color: $non-working-color, $amount: 10%);
         }
     }
+}
+
+.cellTooltip {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: $light-dark;
+
+    p {
+        margin: 0;
+    }
+}
+
+.v-icon {
+    font-size: 15px;
+    color: $focus-color;
 }
 </style>
