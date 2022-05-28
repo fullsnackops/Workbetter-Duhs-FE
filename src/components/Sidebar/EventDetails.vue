299035999b439478d08eaff7891e06f3222712aa
fullsnackops <tbsniper61@gmail.com>
2022-05-28 18:43:09 -0400

    build initial eventdetails component

---
+++ src/components/Sidebar/EventDetails.vue
@@ -0,0 +1,15 @@
+<template>
+    <vue-perfect-scrollbar class="rightsidebar-scroll-area" id="details-scroll" :settings="settings">
+        <div class="inner-toolbar">
+            <v-timeline dense clipped align-top class="pt-0">
+            </v-timeline>
+        </div>
+    </vue-perfect-scrollbar>
+</template>
+
+<script>
+export default {
+    props: ['events', 'settings'],
+}
+</script>
+
