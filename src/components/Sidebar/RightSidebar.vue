2d8eac5507c7a5def5f9f150089e3e659d823f84
fullsnackops <tbsniper61@gmail.com>
2022-05-28 18:34:05 -0400

    add event details component, get position for scroll to current day

--- src/components/Sidebar/RightSidebar.vue
+++ src/components/Sidebar/RightSidebar.vue
@@ -7,6 +7,7 @@
                     {{ $t('message.calendarEvents') }}
                 </v-toolbar-title>
             </v-toolbar>
+            <event-details :events="events" :settings="settings" ref="refDetails"></event-details>
         </v-navigation-drawer>
 
         <div class="right-sidebar">
@@ -18,6 +19,8 @@
 </template>
 
 <script>
+import EventDetails from '@/components/Sidebar/EventDetails'
+
 export default {
     data() {
         return {
@@ -30,6 +33,40 @@ export default {
     methods: {
         toggleSidebar() {
             this.toggle = !this.toggle
+        },
+        getDayForScroll() {
+            // const { timezone } = this.$store.getters.user
+            const timezone = 'America/Chicaco'
+            const today = moment.tz(timezone).format('YYYY-MM-DD')
+
+            let dayForScroll = ''
+            const weekDays = Object.keys(this.events)
+            if (weekDays.length && !weekDays.includes(today)) {
+                // If today and weekDays are on the same year/month
+                if (weekDays[0].substring(0, 7) === today.substring(0, 7)) {
+                    // If today is on the out of weekDays arange
+                    if (
+                        weekDays[0].substring(8, 10) > today.substring(8, 10) ||
+                        weekDays[weekDays.length - 1].substring(8, 10) < today.substring(8, 10)
+                    ) {
+                        dayForScroll = weekDays[0]
+                    } else {
+                        // If today is in the weekDays arange
+                        for (const day of weekDays) {
+                            if (day.substring(8, 10) > today.substring(8, 10)) {
+                                dayForScroll = day
+                                break
+                            }
+                        }
+                    }
+                } else {
+                    // If today and weekDays are on the different year/month
+                    dayForScroll = weekDays[0]
+                }
+            } else {
+                dayForScroll = today
+            }
+            return dayForScroll
         },
     },
     computed: {
