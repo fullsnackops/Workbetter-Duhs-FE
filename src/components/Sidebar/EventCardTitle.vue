187c042802280dcd273be72c9faf112f8b1b8416
fullsnackops <tbsniper61@gmail.com>
2022-06-04 17:32:05 -0400

    computed event title

--- src/components/Sidebar/EventCardTitle.vue
+++ src/components/Sidebar/EventCardTitle.vue
@@ -1,6 +1,10 @@
 <template>
     <v-layout row>
-        <v-flex xs11> </v-flex>
+        <v-flex xs11>
+            <span class="white--text font-sm fw-bold">
+                {{ eventTitle }}
+            </span>
+        </v-flex>
     </v-layout>
 </template>
 
@@ -14,6 +18,19 @@ export default {
             TAG_CRITICAL: AppConfig.meetingTag.critical,
             TAG_PERSONAL: AppConfig.meetingTag.personal,
         }
+    },
+    computed: {
+        eventTitle() {
+            if (this.appointment.title.includes(this.TAG_CRITICAL)) {
+                return this.appointment.title.replace(this.TAG_CRITICAL, '')
+            }
+
+            if (this.appointment.title.includes(this.TAG_PERSONAL)) {
+                return this.appointment.title.replace(this.TAG_PERSONAL, '')
+            }
+
+            return this.appointment.title
+        },
     },
 }
 </script>
