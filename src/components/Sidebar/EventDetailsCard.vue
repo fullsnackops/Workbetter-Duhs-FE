ee92d917518ae162d8c9a29e875155414adbea75
fullsnackops <tbsniper61@gmail.com>
2022-06-05 06:19:39 -0400

    set & clear interval, calc diff mins from event

--- src/components/Sidebar/EventDetailsCard.vue
+++ src/components/Sidebar/EventDetailsCard.vue
@@ -4,4 +4,35 @@
     </div>
 </template>
 
-<script></script>
+<script>
+import moment from 'moment-timezone'
+import { differenceInMinutes } from 'date-fns'
+
+export default {
+    props: ['appointment'],
+    data() {
+        return {
+            interval: undefined,
+            diffMinsFromEventStart: NaN,
+        }
+    },
+    methods: {
+        calcDiffMinsFromEvent() {
+            if (this.diffMinsFromEventStart <= 0) return
+            // const { timezone } = this.$store.getters.user
+            const timezone = 'America/Chicago'
+            this.diffMinsFromEventStart = differenceInMinutes(moment(this.appointment.from), moment.tz(timezone))
+        },
+    },
+    created() {
+        this.calcDiffMinsFromEvent()
+        this.interval = setInterval(this.calcDiffMinsFromEvent, 60 * 1000)
+    },
+    beforeDestroy() {
+        if (this.interval) {
+            clearInterval(this.interval)
+            this.interval = undefined
+        }
+    },
+}
+</script>
